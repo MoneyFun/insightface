f74294caec2bd8cec66fbc2d63863ed7b939dc6f
MoneyFun <1483586698@qq.com>
2019-04-18 14:31:30 +0800

    remove alignment

--- src/train.sh
+++ src/train.sh
@@ -3,13 +3,11 @@ export MXNET_CPU_WORKER_NTHREADS=24
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
 
-DATA_DIR=/opt/jiaguo/faces_vgg_112x112
+DATA_DIR=../datasets/faces_emore/
 
-NETWORK=r50
-JOB=softmax1e3
-MODELDIR="../model-$NETWORK-$JOB"
+NETWORK=y1
+MODELDIR="../model-$NETWORK"
 mkdir -p "$MODELDIR"
 PREFIX="$MODELDIR/model"
 LOGFILE="$MODELDIR/log"
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --prefix "$PREFIX" --per-batch-size 128 > "$LOGFILE" 2>&1 &
-
+CUDA_VISIBLE_DEVICES='' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --prefix "$PREFIX" --per-batch-size 128
