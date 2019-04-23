d29354d85986a148487415398a417db38ff0cf4e
1012041750@qq.com <1012041750@qq.com>
2019-04-23 11:27:27 +0800

    remove codes from deploy

--- deploy/face_model.py
+++ deploy/face_model.py
@@ -16,8 +16,6 @@ from sklearn.decomposition import PCA
 from time import sleep
 from easydict import EasyDict as edict
 from mtcnn_detector import MtcnnDetector
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'common'))
-# import face_image
 import face_preprocess
 
 
